9e83954a9eca5786a12572db2759fd31e621f2f1
hadamssmith <Ajetunmobiadam@gmail.com>
2019-05-14 16:09:47 +0100

    updated with setter and getter method

--- Book.java
+++ Book.java
@@ -6,6 +6,27 @@ public class Book {
     private int pages;
     private Boolean hardCover; 
     private String publisher;
+
+    public void setAuthor(String author){
+        this.author = author; 
+    }
+
+    public String getAuthor(){
+        return author;
+    }
+    public void setPages(int pages) {
+        this.pages = pages;
+    }
+
+    public int getPages() {
+        return pages;    }
+    public void setIsHardCover(){
+        this.isHardCover = true;
+    } 
+    public boolean getIsHardCover() {
+        return true;
+    }
+
     
 
     public static void main(String... args) {
