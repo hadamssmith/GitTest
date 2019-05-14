60798973f89ef6f78d742582149d5f60c3f249b0
hadamssmith <Ajetunmobiadam@gmail.com>
2019-05-14 14:57:14 +0100

    first commit

---
+++ Book.java
@@ -0,0 +1,18 @@
+//Name:Ajetunmobi Adam
+
+public class Book {
+
+    private String author;
+    private int pages;
+    private Boolean hardCover; 
+    private String publisher;
+    
+
+    public static void main(String... args) {
+
+        
+        
+
+
+    }
+}
