5906e776d363914b88774f918826dbbca970e418
hadamssmith <Ajetunmobiadam@gmail.com>
2019-05-15 11:45:41 +0100

    How to create a table in using java

---
+++ creatingtable.java
@@ -0,0 +1,13 @@
+// creating a table 
+// Name: Ajetunmobi Adam
+//Date: 24/04/2019
+public class creatingtable {
+
+    public static void main(String[] args) {
+        System.out.println("\n Company name\t Year\t Gross profit\t Net income\n Semicolon\t2018\t 1500000\t 1000000\n Asuqoo\t        2018\t 3000000\t 2500000");
+        System.out.println(" paystack\t2018\t 4500000\t 3915000");
+        System.out.println("\n          1\n         1 2\n        1 2 3\n       1 2 3 4\n      1 2 3 4 5\n     1 2 3 4 5 6");
+        
+        System.out.println("hellois fun \rjava ");
+    }// end methodmain
+}//end creatingtable
